f4266829a0832556a2d05236aacf0c54d7515447
halil.ozkan <halil.ozkan@verapi.com>
2019-05-03 21:56:47 +0300

    ABYSSP-455 Api traffic data model changes

--- src/main/java/com/verapi/abyss/common/message/ApiTraffic.java
+++ src/main/java/com/verapi/abyss/common/message/ApiTraffic.java
@@ -13,15 +13,30 @@ package com.verapi.abyss.common.message;
 
 public class ApiTraffic {
 
-    public static final String ID = "id";
+    public static final String DAY = "day";
+    public static final String TIMESTAMP = "timestamp";
+    public static final String API_ID = "api_id";
+    public static final String APP_ID = "app_id";
     public static final String ACCEPT_ENCODING = "accept_encoding";
+    public static final String API = "api";
+    public static final String APP = "app";
     public static final String CONTENT_LENGTH = "content_length";
     public static final String CONTENT_TYPE = "content_type";
+    public static final String CONTRACT = "contract";
+    public static final String CONTRACT_ID = "contract_id";
     public static final String COOKIES = "cookies";
+    public static final String CORRELATION_ID = "correlation_id";
+    public static final String COUNTRY = "country";
+    public static final String COUNTRY_ID = "country_id";
     public static final String CREATED = "created";
     public static final String HTTP_METHOD = "http_method";
     public static final String HTTP_SESSION = "http_session";
     public static final String HTTP_VERSION = "http_version";
+    public static final String ID = "id";
+    public static final String LICENSE = "license";
+    public static final String LICENSE_ID = "license_id";
+    public static final String ORGANIZATION = "organization";
+    public static final String ORGANIZATION_ID = "organization_id";
     public static final String REFERER = "referer";
     public static final String REMOTE_CLIENT = "remote_client";
     public static final String REQUEST_BODY = "request_body";
@@ -39,7 +54,7 @@ public class ApiTraffic {
     public static final String RESPONSE_BYTES_WRITTEN = "response_bytes_written";
     public static final String RESPONSE_HEADERS = "response_headers";
     public static final String RESPONSE_STATUS_CODE = "response_status_code";
-    public static final String TIMESTAMP = "timestamp";
+    public static final String ROUTE = "route";
     public static final String USER_AGENT = "user_agent";
     public static final String USERNAME = "username";
 
